5f416081ae2db686f0a4133dc5c306e70b724442
Ram Nayudu <Ram.Nayudu@quisitive.com>
2020-09-16 18:45:40 +0530

    GetMovie API

--- MoviesMicroService/MovieFunction.cs
+++ MoviesMicroService/MovieFunction.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using Microsoft.Azure.Cosmos;
+using MoviesMicroService.Models;
+using System.Linq;
 
 namespace MoviesMicroService
 {
@@ -20,24 +22,23 @@ namespace MoviesMicroService
             this.movieContainer = movieContainer;
         }
 
-        [FunctionName("Function1")]
+        [FunctionName("GetMovies")]
         public async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
+             [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
+             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            try
+            {
+                log.LogInformation("Movie function started execution");
+                var responseMessage = movieContainer.GetItemLinqQueryable<Movie>(true).AsEnumerable().ToList();
 
-            string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
+                return new OkObjectResult(responseMessage);
+            }
+            catch (Exception e)
+            {
+                log.LogInformation(" Exception while executing Movie function ");
+                return new BadRequestResult();
+            }
         }
     }
 }
