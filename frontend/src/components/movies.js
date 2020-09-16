ae75df5c3bb3582bf85be0b64dac30d0e598f8b9
Ram Nayudu <Ram.Nayudu@quisitive.com>
2020-09-16 19:32:00 +0530

    Added Colums and Options for movies table with search and filter options

--- frontend/src/components/movies.js
+++ frontend/src/components/movies.js
@@ -12,9 +12,66 @@ class Movies extends React.Component {
       data: [],
     }
   }
-
   
   render() {
+
+    const columns = [
+      { 
+        name: "imdbID",
+        label: "IMDBID",
+        options: {
+         filter: true,
+         sort: true,
+        }
+       },
+      { 
+       name: "title",
+       label: "Title",
+       options: {
+        filter: true,
+        sort: true,
+       }
+      },
+      {
+       name: "location",
+       label: "Location",
+       options: {
+        filter: true,
+        sort: false,
+       }
+      },
+      {
+       name: "language",
+       label: "Language",
+       options: {
+        filter: true,
+        sort: false,
+       }
+      },
+      {
+       name: "imdbRating",
+       label: "IMDBRating",
+       options: {
+        filter: true,
+        sort: false,
+       }
+      },
+     ];
+
+    const options = {
+      filter: true,
+      filterType: 'dropdown',
+      responsive: 'vertical',
+      selectToolbarPlacement:'none',
+      onRowClick: (rowData, rowState) => {
+        this.props.history.push({ 
+          pathname: '/movie',
+          state: rowData[0]
+         });
+
+      },
+      enableNestedDataAccess: '.', // allows nested data separated by "." (see column names and the data structure above)
+    };
 
     return (
       <React.Fragment>
