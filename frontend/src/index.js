a564c69486237d16ae73163df6ba911f87b6cf34
Ram Nayudu <Ram.Nayudu@quisitive.com>
2020-09-16 19:24:27 +0530

    Frontend React project

---
+++ frontend/src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import Movies from './components/movies';
+import Movie from './components/movie';
+import { Route, Link, BrowserRouter } from 'react-router-dom'
+import Sample from './components/sample';
+
+const routs = (
+  < BrowserRouter >
+        <Route exact path="/movies" component={Movies} />
+        <Route exact path="/movie" component={Movie} />
+  </ BrowserRouter >
+);
+
+ReactDOM.render(
+routs,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
